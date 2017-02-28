e3df6782ead51eed962d0ff7fe5ced39ad1a3dea
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-02-28 12:47:16 -0500

    Sugarloaf Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -463,6 +463,65 @@ function renderPromoDetails(container, template, collection){
     });
     $(container).html(item_rendered.join(''));
 }
+
+function renderStoreDetails(container, template, collection, slug){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    item_list.push(collection);
+    $.each( item_list , function( key, val ) {
+        if ((val.store_front_url).indexOf('missing.png') > -1){
+            val.alt_store_front_url = "//www.mallmaverick.com/photos/original/missing.png";
+        } else {
+            val.alt_store_front_url = getImageURL(val.store_front_url); 
+        }
+        val.category_list = getCategoriesNamesByStoreSlug(slug);
+        val.map_x_coordinate = val.x_coordinate - 19;
+        val.map_y_coordinate = val.y_coordinate - 58;
+        val.property_map = getPropertyDetails().mm_host + getPropertyDetails().map_url;
+        if (val.website != null && val.website.length > 0){
+            val.show = "display:inline-block";
+        }
+        else{
+            val.show = "display:none";
+        }
+        if (val.phone != null && val.phone.length > 0){
+            val.phone_show = "display:inline-block";
+        }
+        else{
+            val.phone_show = "display:none";
+        }
+        
+        if (val.twitter != null && val.twitter.length > 0){
+            val.twitter_show = "display:inline-block";
+        }
+        else{
+            val.twitter_show = "display:none";
+        }
+        
+        if (val.twitter_show == "display:none" && val.phone_show == "display:none" ){
+            val.show_line = "display:none";
+        }
+        else{
+            val.show_line = "display:block";
+        }
+        if((val.twitter == null || val.twitter == "") && (val.facebook == "" || val.facebook == null)){
+            val.hide_social = "display:none;";
+        }
+        if (val.facebook != null && val.facebook.length > 0){
+            val.facebook_show = "display:inline-block";
+        }
+        else{
+            val.facebook_show = "display:none";
+        }
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).show();
+    $(container).html(item_rendered.join(''));
+}
+
 function renderStoreList(container, template, collection, type){
     var item_list = [];
     var item_rendered = [];
@@ -517,64 +576,6 @@ function renderStoreList(container, template, collection, type){
         val.block = current_initial + '-block';
         var rendered = Mustache.render(template_html,val);
         var upper_current_initial = current_initial.toUpperCase();
-        item_rendered.push(rendered);
-    });
-    $(container).show();
-    $(container).html(item_rendered.join(''));
-}
-
-function renderStoreDetails(container, template, collection, slug){
-    var item_list = [];
-    var item_rendered = [];
-    var template_html = $(template).html();
-    Mustache.parse(template_html);   // optional, speeds up future uses
-    item_list.push(collection);
-    $.each( item_list , function( key, val ) {
-        if ((val.store_front_url).indexOf('missing.png') > -1){
-            val.alt_store_front_url = "//www.mallmaverick.com/photos/original/missing.png";
-        } else {
-            val.alt_store_front_url = getImageURL(val.store_front_url); 
-        }
-        val.category_list = getCategoriesNamesByStoreSlug(slug);
-        val.map_x_coordinate = val.x_coordinate - 19;
-        val.map_y_coordinate = val.y_coordinate - 58;
-        val.property_map = getPropertyDetails().mm_host + getPropertyDetails().map_url;
-        if (val.website != null && val.website.length > 0){
-            val.show = "display:inline-block";
-        }
-        else{
-            val.show = "display:none";
-        }
-        if (val.phone != null && val.phone.length > 0){
-            val.phone_show = "display:inline-block";
-        }
-        else{
-            val.phone_show = "display:none";
-        }
-        
-        if (val.twitter != null && val.twitter.length > 0){
-            val.twitter_show = "display:inline-block";
-        }
-        else{
-            val.twitter_show = "display:none";
-        }
-        
-        if (val.twitter_show == "display:none" && val.phone_show == "display:none" ){
-            val.show_line = "display:none";
-        }
-        else{
-            val.show_line = "display:block";
-        }
-        if((val.twitter == null || val.twitter == "") && (val.facebook == "" || val.facebook == null)){
-            val.hide_social = "display:none;";
-        }
-        if (val.facebook != null && val.facebook.length > 0){
-            val.facebook_show = "display:inline-block";
-        }
-        else{
-            val.facebook_show = "display:none";
-        }
-        var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
     $(container).show();
