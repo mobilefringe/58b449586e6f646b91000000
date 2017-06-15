274a3d706ec01a84294fce7b313351e8ed3aeb7b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-15 13:53:23 -0400

    Sugarloaf Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -407,7 +407,7 @@ function renderStoreDetails(container, template, collection, slug){
     item_list.push(collection);
     $.each( item_list , function( key, val ) {
         if ((val.store_front_url).indexOf('missing.png') > -1){
-            val.alt_store_front_url = "//www.mallmaverick.com/photos/original/missing.png";
+            val.alt_store_front_url = "//codecloud.cdn.speedyarils.net/sites/58b449586e6f646b91000000/image/jpeg/1488210736000/sugarlogo.jpg";
         } else {
             val.alt_store_front_url = getImageURL(val.store_front_url); 
         }
