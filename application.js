ba55344c56b0d3de91a5824575ffdcab492dc4f4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-15 14:10:37 -0400

    Sugarloaf Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -163,31 +163,31 @@ function renderHours(container, template, collection, type){
     $(container).html(item_rendered.join(''));
 }
 
-function renderHomeHours(container, template, collection){
-    var today_hours = getTodaysHours();
-    var item_list = [];
-    var item_rendered = [];
-    var template_html = $('#home_hours_template').html();
-    Mustache.parse(template_html);   // optional, speeds up future uses
-    item_list.push(today_hours);    
-    $.each(item_list, function(key, val) {
-        val.day = moment().date();
-        var d = moment();
-        val.month = moment().month();
-        val.weekday = moment().format("dddd");
-        if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
-            var open_time = moment(val.open_time).tz(getPropertyTimeZone());
-            var close_time = moment(val.close_time).tz(getPropertyTimeZone());
-            val.h = open_time.format("h:mma") + " - " + close_time.format("h:mma");
-            val.close_time = close_time.format("h:mma");
-        } else {
-            val.h = "Closed";
-        }
-        var rendered = Mustache.render(template_html,val);
-        item_rendered.push(rendered);
-    });
-    $('#home_hours_container').html(item_rendered.join(''));
-}
+// function renderHomeHours(container, template, collection){
+//     var today_hours = getTodaysHours();
+//     var item_list = [];
+//     var item_rendered = [];
+//     var template_html = $('#home_hours_template').html();
+//     Mustache.parse(template_html);   // optional, speeds up future uses
+//     item_list.push(today_hours);    
+//     $.each(item_list, function(key, val) {
+//         val.day = moment().date();
+//         var d = moment();
+//         val.month = moment().month();
+//         val.weekday = moment().format("dddd");
+//         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
+//             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
+//             var close_time = moment(val.close_time).tz(getPropertyTimeZone());
+//             val.h = open_time.format("h:mma") + " - " + close_time.format("h:mma");
+//             val.close_time = close_time.format("h:mma");
+//         } else {
+//             val.h = "Closed";
+//         }
+//         var rendered = Mustache.render(template_html,val);
+//         item_rendered.push(rendered);
+//     });
+//     $('#home_hours_container').html(item_rendered.join(''));
+// }
 
 function renderHomeHours(container, template, collection){
     var current_local = getStorage().primary_locale;
