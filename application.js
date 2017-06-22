a5677e40b8ff43cac1b5070737a2cc1f3c06c7bd
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-22 09:31:13 -0400

    Sugarloaf Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -131,7 +131,7 @@ function renderHours(container, template, collection, type){
                 }
                 if(Cookies.get('current_locale') == "fr-CA"){
                     // var holiday = moment(val.holiday_date).tz(getPropertyTimeZone());
-                    var french_holiday = moment(holiday).locale('fr');
+                    var french_holiday = holiday.locale('fr');
                     val.formatted_date = moment(french_holiday).locale('fr-ca').format("DD MMM");
                     if (val.open_time && val.close_time && val.is_closed == false){
                         var open_time = moment(val.open_time).tz(getPropertyTimeZone());
